abc2e89302b725a9f84b4119ee39d8773dbf3675
Satheesh Doosa <satee143@gmail.com>
2025-05-16 23:43:06 +0530

    removed application

--- vpc-testing/provider.tf
+++ vpc-testing/provider.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.84.0"
+      version = "5.81.0"
     }
   }
   backend "s3" {
     bucket       = "terraform-module-testing-aws"
     region       = "us-east-1"
-    key          = "vpc-statefile"
+    key          = "vpc-statefile-new"
+    dynamodb_table = "myredbucket222"
     #use_lockfile = "true"
   }
 }
