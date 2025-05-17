cf955c63ac041e069ae02790af4d79bae03c6932
Satheesh Doosa <satee143@gmail.com>
2025-05-18 00:40:34 +0530

    Added code sg

--- sg-testing/provider.tf
+++ sg-testing/provider.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.8.4"
+      version = "5.84.0"
     }
   }
   backend "s3" {
     bucket       = "terraform-module-testing-aws"
     region       = "us-east-1"
     key = "sg-statefile"
-    #dynamodb_table = "myredbucket222"
-    use_lockfile = "true"
+    dynamodb_table = "myredbucket222"
+    #use_lockfile = "true"
   }
 }
 
